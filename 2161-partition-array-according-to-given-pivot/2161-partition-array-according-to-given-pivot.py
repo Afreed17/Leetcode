adc6bd5b1434ee2ad9ba5ce22654ee29ad33c0b6
Mohamed Afreed <113635346+Afreed17@users.noreply.github.com>
2024-11-11 20:31:18 +0530

    Time: 23 ms (86.83%), Space: 33.5 MB (62.31%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -2,17 +2,14 @@ class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
         smList = []
         gtList = []
-        eqList = []
-        
-        
+        eqList = []       
         for el in nums:
             if el > pivot:
                 gtList.append(el)
             elif el < pivot:
                 smList.append(el)
             else:
-                eqList.append(el)
-                
+                eqList.append(el)            
         return smList + eqList + gtList
                 
                 
